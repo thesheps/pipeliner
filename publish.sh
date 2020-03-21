790c4977041b11e7f6d32367350c64b0bde9a299
Chris Shepherd <thesheps@gmail.com>
2020-03-21 16:14:48 +0000

    feat(docker): removes npm stuff

--- publish.sh
+++ publish.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-lerna publish
 git submodule foreach git add --all 
 git submodule foreach git commit -m 'feat(bump)'
 git add .
 git commit -m 'bump'
 git push
-lerna exec -- npm publish --access public
