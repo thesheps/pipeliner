26860fad7d919347f5fd7235b665b7ddfb4615ef
Chris Shepherd <thesheps@gmail.com>
2020-04-04 10:02:33 +0100

    feat(schema): bigint

--- db/src/migrations/20200321120447-create-user.ts
+++ db/src/migrations/20200321120447-create-user.ts
@@ -5,29 +5,29 @@ export default {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.BIGINT.UNSIGNED,
       },
       emailAddress: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable("Users");
-  }
+  },
 };
